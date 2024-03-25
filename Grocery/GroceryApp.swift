5664b3b5fe4ef57c2aefd660e1ebc2d5cb3cc535
Simonadp0 <148473832+Simonadp0@users.noreply.github.com>
2024-03-25 17:32:16 +0100

    Initial Commit

---
+++ Grocery/GroceryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryApp.swift
+//  Grocery
+//
+//  Created by Simona De Pasquale on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct GroceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
